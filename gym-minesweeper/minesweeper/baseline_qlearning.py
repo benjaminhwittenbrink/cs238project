ffbe6f03dcb0165c394758d0e28ea868a02a62a2
Benjamin Wittenbrink <witten@stanford.edu>
2023-03-15 09:03:01 -0700

    filter selected actions

--- gym-minesweeper/minesweeper/baseline_qlearning.py
+++ gym-minesweeper/minesweeper/baseline_qlearning.py
@@ -1,5 +1,7 @@
 import minesweeper as ms
 import numpy as np 
+from tqdm import tqdm 
+import pickle
 
 import ExplorationHelpers as ExpHelpers
 
@@ -22,26 +24,36 @@ env = ms.MinesweeperEnv()
 actions = env.action_space
 state = env.reset()
 
-for i in range(NUM_ITERS):
+for i in tqdm(range(NUM_ITERS)):
     
     if i % 100_000 == 0: 
         print(i)
         print("State space size:", len(Q))
         print("Game length:", len(wins))
+        print("Average win (last 100k):", np.mean(wins[-100_000:]))
         print("Average win (last 10k):", np.mean(wins[-10_000:]))
         print("eps:", EPSILON)
+
+        if i % 1_000_000: 
+            pickle.dump(Q, open(f"q_learning_data_55_p2/Q_{i}_p2.pkl", "wb"))
+        np.save(f"q_learning_data_55_p2/wins_{i}_p2.npy", wins)
         wins = []
+        verbose = True
 
     done = False 
     while not done: 
-
         state_str = ms.board2str(state)
         if state_str not in Q: 
             Q[state_str] = np.zeros((ms.BOARD_SIZE, ms.BOARD_SIZE))
+        
+        Q_valid_actions = np.copy(Q[state_str])
+        Q_valid_actions[state != -2] = - np.inf 
 
-        action_x, action_y = ExpHelpers.epsilon_greedy_exploration(env, actions, Q[state_str], EPSILON)
+        action_x, action_y = ExpHelpers.epsilon_greedy_exploration(env, actions, Q_valid_actions, EPSILON)
+        if verbose: print(action_x, action_y)
 
         next_state, reward, done, info = env.step((action_x, action_y))
+        if verbose: print(next_state)
 
         curr_Q = Q[state_str][action_x, action_y]
         update = reward + GAMMA * (np.max(Q[state_str]) - curr_Q)
@@ -51,13 +63,16 @@ for i in range(NUM_ITERS):
             if reward > 0: wins.append(1)
             else: wins.append(0)
             state = env.reset()
+            if verbose: print("win:", reward > 0)
         else: 
             state = next_state
+        
     
+    verbose = False
     if i > 500_000: 
         EPSILON = ExpHelpers.epsilon_decay_schedule(i - 500_000, MAX_EPS, MIN_EPS, EPS_DECAY)
 
 
 print("Game length:", len(wins))
 print("Average win:", np.mean(wins))
-np.save("Q.npy", Q)
+
